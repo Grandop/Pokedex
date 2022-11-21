5c050ed5c72b3932ed01ffa95add0631311bf25b
Pedro Grando <grandop03@gmail.com>
2022-11-21 15:11:30 -0300

    Fixing some bugs

--- Pokedex/Controllers/DetailViewController.swift
+++ Pokedex/Controllers/DetailViewController.swift
@@ -40,7 +40,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         let backgroundColor = UIColor(named: (pokemon?.type[0].lowercased())!)
-        firstView.backgroundColor = backgroundColor?.darker()
+        firstView.backgroundColor = backgroundColor?.withAlphaComponent(0.8)
         pokemonName.text = pokemon?.name.english
         pokemonImage.image = UIImage(named: String(format: "%03d", pokemon!.id))
         
@@ -98,33 +98,4 @@ class DetailViewController: UIViewController {
     
 }
 
-extension UIColor {
-    private func makeColor(componentDelta: CGFloat) -> UIColor {
-        var red: CGFloat = 0
-        var blue: CGFloat = 0
-        var green: CGFloat = 0
-        var alpha: CGFloat = 0
-    
-        getRed(
-            &red,
-            green: &green,
-            blue: &blue,
-            alpha: &alpha
-        )
-    
-        return UIColor(
-            red: add(componentDelta, toComponent: red),
-            green: add(componentDelta, toComponent: green),
-            blue: add(componentDelta, toComponent: blue),
-            alpha: alpha
-        )
-    }
 
-    private func add(_ value: CGFloat, toComponent: CGFloat) -> CGFloat {
-            return max(0, min(1, toComponent + value))
-        }
-    
-    func darker(componentDelta: CGFloat = 0.1) -> UIColor {
-            return makeColor(componentDelta: -1 * componentDelta)
-        }
-}
